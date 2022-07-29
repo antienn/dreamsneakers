87d4bf3c398abcfe406c9476dbb459ee877ef70e
Trần An Tiến <betientroll12@gmail.com>
2022-07-29 13:08:39 +0700

    add

--- js/showshoe.js
+++ js/showshoe.js
@@ -10,7 +10,7 @@ const showShoe = (page, listShoee, root) => {
                 <p class="current_price">$${shoe.price}.00</p>
                 <p class="old_price">$${shoe.oldprice}.00</p>
             </div>
-            <div class="btn_buy ${shoe.soldout == true ? 'soldOut': ''}">
+            <div class="btn_buy ${shoe.soldout == true ? 'soldOut': 'canbuy'}" id="${shoe.id}">
                 <i class="fa-solid fa-cart-shopping"></i>
             </div>
             <div class="big_view" id="${shoe.id}">
@@ -18,9 +18,11 @@ const showShoe = (page, listShoee, root) => {
             </div>
         </div>
     `).join("")
+    //quickview
     tmpImg = document.createElement('img');
     document.querySelectorAll('.big_view').forEach(f => f.addEventListener('click', () => {
         blackPage.classList.add("active")
+        blackPage.style.transformOrigin ="top"
         for(let i = 0; i < listShoee.length;i++){
             if(listShoee[i].id - 1 == f.id-1){
                 tmpImg.setAttribute('src',listShoee[i].image,);
@@ -30,10 +32,23 @@ const showShoe = (page, listShoee, root) => {
     })) 
     tmpImg.onclick = () =>{
         blackPage.removeChild(tmpImg);
-    } 
+    }
+
+    document.querySelectorAll('.btn_buy.canbuy').forEach(f => f.addEventListener('click', () => {
+        if(curUserr){
+            localStorage.setItem("curItem",JSON.stringify(f.id));
+            localStorage.setItem("curUser",JSON.stringify(curUserr));
+            window.location.href="./info.html";
+        }else{
+            window.location.href="./login.html";
+        }
+    })) 
+
 }
 
 showShoe(0,listShoe,document.querySelector('.content__bestSeller'));
 
-
-// renderFilm(0, sortView(listFilm), document.querySelector('.content__top-view'))
+var count = document.querySelectorAll(".btn_buy.soldOut");
+    for (let i = 0; i < count.length; i++) {
+        count[i].innerHTML = "Sold Out";
+    }
